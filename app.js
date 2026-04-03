104b9eb4a91d478c809f81638c990c06ac146603
ssabir94 <sanasabir94@gmail.com>
2026-04-03 14:58:17 +0200

    Aplico el slugi

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.use('/', products_routes)
 
 //Server startup
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
+    console.log(slugify('server is listening on port 5000', {
+  replacement: '*'
+}));
 })
 
 
